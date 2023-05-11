473cf863eae54e3b89d4959a0633f4eced53088e
MaryVanna <MashFijhoa95@yandex.ru>
2023-05-11 22:52:23 +0300

    lint fixes

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,8 +1,8 @@
-import genDiff from '../index.js';
-import { fileURLToPath } from 'url';
-import { dirname } from 'node:path';
 import fs from 'node:fs';
-import path from 'node:path';
+import { dirname } from 'node:path';
+import path from 'path';
+import { fileURLToPath } from 'url';
+import genDiff from '../index.js';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
