02199c4d17ec579946e4a25d34a043be48d7bb1c
MaryVanna <MashFijhoa95@yandex.ru>
2023-05-14 16:38:04 +0300

    hexlet-check fixes

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -3,10 +3,13 @@ import { program } from 'commander';
 import genDiff from '../index.js';
 
 program
+  .name('gendiff')
   .description('Compares two configuration files and shows a difference.')
-  .version('0.0.1')
+  .version('1.0.0')
   .option('-f, --format <type>', 'output format', 'stylish')
   .arguments('<path1> <path2>')
-  .action((path1, path2, options) => console.log(genDiff(path1, path2, options.format)));
+  .action((path1, path2, options) => {
+    console.log(genDiff(path1, path2, options.format))
+  });
 
 program.parse();
