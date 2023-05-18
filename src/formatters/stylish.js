a8721f0a8b2dfd4cb543272598c4847d7c4555b4
MaryVanna <MashFijhoa95@yandex.ru>
2023-05-18 23:47:52 +0300

    maintainability fixes

--- src/formatters/stylish.js
+++ src/formatters/stylish.js
@@ -4,25 +4,25 @@ const stylish = (data) => {
   const iter = (keys, depth) => {
     const currentSpasing = '    '.repeat(depth);
     const strings = keys.map(({
-        key, value, newValue, children, status,
-      }) => {
-        const valueToString = (val) => `${key}: ${_.isObject(val) ? iter(children, depth + 1) : val}`;
-        switch (status) {
-          case 'unchanged':
+      key, value, newValue, children, status,
+    }) => {
+      const valueToString = (val) => `${key}: ${_.isObject(val) ? iter(children, depth + 1) : val}`;
+      switch (status) {
+        case 'unchanged':
+          return `${currentSpasing}    ${valueToString(value)}`;
+        case 'added':
+          return `${currentSpasing}  + ${valueToString(value)}`;
+        case 'removed':
+          return `${currentSpasing}  - ${valueToString(value)}`;
+        case 'updated':
+          if (_.isObject(value) && _.isObject(newValue)) {
             return `${currentSpasing}    ${valueToString(value)}`;
-          case 'added':
-            return `${currentSpasing}  + ${valueToString(value)}`;
-          case 'removed':
-            return `${currentSpasing}  - ${valueToString(value)}`;
-          case 'updated':
-            if (_.isObject(value) && _.isObject(newValue)) {
-              return `${currentSpasing}    ${valueToString(value)}`;
-            }
-            return [`${currentSpasing}  - ${valueToString(value)}`, `${currentSpasing}  + ${valueToString(newValue)}`];
-          default:
-            throw new Error('Упс, что-то пошло не так [✖‿✖]');
-        }
-      });
+          }
+          return [`${currentSpasing}  - ${valueToString(value)}`, `${currentSpasing}  + ${valueToString(newValue)}`];
+        default:
+          throw new Error('Упс, что-то пошло не так [✖‿✖]');
+      }
+    });
     return `{\n${strings.flat().join('\n')}\n${currentSpasing}}`;
   };
 
